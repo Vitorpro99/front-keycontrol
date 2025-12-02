d95b7acd75a611ef0629da1fd29ad551e4d5c621
Vitorpro99 <vitormateusilva4@gmail.com>
2025-12-01 21:26:16 -0300

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,29 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8000/",
-    headers:{
-        "Content-Type": "application/json",
+    // A MÁGICA ACONTECE AQUI:
+    // Tenta ler a variável da Vercel. Se não existir (null/undefined), usa o localhost.
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/",
+    headers: {
+        "Content-type": "application/json"
     }
-})
+});
 
+// Interceptor para adicionar o Token automaticamente
 apiClient.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            config.headers["Authorization"] = 'Bearer ' + token;
+        // O localStorage só existe no navegador, então verificamos se window está definido
+        if (typeof window !== 'undefined') {
+            const token = localStorage.getItem("token");
+            if (token) {
+                config.headers["Authorization"] = 'Bearer ' + token;
+            }
         }
-        
         return config;
     },
     (error) => {
         return Promise.reject(error);
     }
 );
+
 export default apiClient;
